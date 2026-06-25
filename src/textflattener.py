b7a61459c62f7fc2b1d598dd73b3bfc40ac9f392
Brendan Chuy <brendanchuy1@gmail.com>
2026-06-25 19:03:56 +0000

    text flattener and flattened classes implemented

---
+++ src/textflattener.py
@@ -0,0 +1,68 @@
+import os
+import ftfy
+import html2text
+from email import message_from_string
+from unstructured.partition.auto import partition
+from unstructured.cleaners.core import (
+    clean,
+    group_broken_paragraphs,
+    replace_unicode_quotes,
+    bytes_string_to_string
+)
+from flattenedtext import FlattenedText
+
+class TextFlattener:
+    def flatten(self, raw_input: str) -> FlattenedText:
+        original_text = raw_input
+
+        if os.path.exists(raw_input):
+            raw_input = self.extract_text_from_document(raw_input)
+
+        clean_text = self.fix_encoding(raw_input)
+
+        if self.looks_like_html(clean_text):
+            clean_text = self.convert_html(clean_text)
+
+        clean_text = self.clean_whitespace(clean_text)
+
+        return FlattenedText(original_text, clean_text)
+
+    def extract_text_from_document(self, file_path: str) -> str:
+        elements = partition(filename=file_path) # Unstructured parses text from file
+        return "\n".join(str(element) for element in elements)
+
+    def fix_encoding(self, text: str) -> str:
+        text = ftfy.fix_text(text)
+        text = bytes_string_to_string(text)
+        text = replace_unicode_quotes(text)
+        return text
+
+    def looks_like_html(self, text: str) -> bool:
+        lowered = text.lower()
+
+        return (
+            "<html" in lowered
+            or "<body" in lowered
+            or "<p>" in lowered
+            or "</p" in lowered
+            or "<a " in lowered
+            or "</div" in lowered
+        )
+
+    def convert_html(self, text: str) -> str:
+        return html2text.html2text(text)
+
+    def clean_whitespace(self, text: str) -> str:
+        text = group_broken_paragraphs(text)
+
+        text = clean(
+            text,
+            bullets=False,
+            extra_whitespace=True,
+            dashes=True,
+            trailing_punctuation=False,
+            lowercase=False
+        )
+
+        return text.strip()
+    
