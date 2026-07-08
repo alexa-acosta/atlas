617b0ff4d6a3e58a04527a95a9a5c75f32300f27
Brendan Chuy <chuybrendan@gmail.com>
2026-07-08 14:12:53 -0400

    API routes for atlasscanner.scan(), scans list, and scan history select scan implemented. Some helper methods added to database.py for API routes, adjusted atlasscanner.py for a scan_id.

---
+++ react-with-flask/src/pages/History.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+
+export default function History() {
+  return (
+    <div className="page">
+      <Navbar loggedIn />
+    </div>
+  );
+}
--- react-with-flask/src/utils/auth.js
+++ react-with-flask/src/utils/auth.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5000";
+const API = "";
 
 export async function login(email, password) {
   const res = await fetch(`${API}/api/login`, {
