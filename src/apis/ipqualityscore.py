0d30c3ce0760e435c9d32243b9d7d0e1cda8663a
tt3092 Tetiana Tsarenko <tt3092@columbia.edu>
2026-07-09 03:22:43 +0000

    fix IPQualityScore email so it doesn't crash on bad responses

--- src/apis/ipqualityscore.py
+++ src/apis/ipqualityscore.py
@@ -1,16 +1,18 @@
 import os
 import requests
+from urllib.parse import quote 
 
 class IPQualityScore:
   def __init__(self):
     self.api_key = os.getenv('IPQS_API_KEY')
   
   def check_email(self, email: str):
-    url = f"https://www.ipqualityscore.com/api/json/email/{self.api_key}/{email}"
+    url = f"https://www.ipqualityscore.com/api/json/email/{self.api_key}/{quote(email)}"
     response = requests.get(url)
 
-    if not response:
+    try:
+      return response.json()
+    except Exception:
       return {}
-    return response.json()
 
   
